122a2cfed03cb187d3385549ad68fad5487d257a
Alejandro Kyle Guevarra <guevarrakyle@yahoo.com.ph>
2023-06-21 01:44:54 +0800

    Added search

--- client/inventory-system/src/App.jsx
+++ client/inventory-system/src/App.jsx
@@ -77,15 +77,33 @@ function App() {
     );
   }
 
+  const filterTable = (e) => {
+    const filter = e.target.value.toUpperCase();
+    const tr = document.getElementsByTagName('tr');
+    for (let i = 0; i < tr.length; i++) {
+      const td = tr[i].getElementsByTagName('td')[0];
+      if (td) {
+        const textValue = td.textContent || td.innerHTML;
+        if (textValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = '';
+        } else {
+          tr[i].style.display = 'none';
+        }
+      }
+    }
+  }
+
+
   return (
     <div className="App">
       <h1>Inventory System</h1>
       <form className='form' onSubmit={e => addItem(e)}>
-        <input type="text" placeholder='Item Name' onChange={e => {setItemName(e.target.value)}} value={itemName} required />
-        <input type="text" placeholder='Item Price' onChange={e => {setItemPrice(e.target.value)}} value={itemPrice} required />
-        <input type="number" placeholder='Item Quantity' onChange={e => {setItemQuantity(e.target.value)}} value={itemQuantity} min="0" required />
-        <button type="submit">Add</button>
+        <input className='new-input' type="text" placeholder='Item Name' onChange={e => {setItemName(e.target.value)}} value={itemName} required />
+        <input className='new-input' type="text" placeholder='Item Price' onChange={e => {setItemPrice(e.target.value)}} value={itemPrice} required />
+        <input className='new-input' type="number" placeholder='Item Quantity' onChange={e => {setItemQuantity(e.target.value)}} value={itemQuantity} min="0" required />
+        <button className='new-button' type="submit">Add</button>
       </form>
+      <input className='search-input' type="text" placeholder='Search Item' onChange={e => filterTable(e)} />
       <table className="todo-listItems">
         <thead>
           <tr>
